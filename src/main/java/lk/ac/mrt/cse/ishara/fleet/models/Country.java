7eb0cc97a1eddf3c20959c0b1577388dcf2c8670
IsharaU <wellahewawiud.20@uom.lk>
2024-09-29 23:53:23 +0530

    add state class

--- src/main/java/lk/ac/mrt/cse/ishara/fleet/models/Country.java
+++ src/main/java/lk/ac/mrt/cse/ishara/fleet/models/Country.java
@@ -2,13 +2,12 @@ package lk.ac.mrt.cse.ishara.fleet.models;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -24,6 +23,9 @@ public class Country {
     private String description;
     private String nationality;
     private String continent;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
 
 
 }
