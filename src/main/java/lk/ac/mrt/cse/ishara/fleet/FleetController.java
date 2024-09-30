0b85330679be08151a119cf1e2c128d0c9dae8ef
IsharaU <wellahewawiud.20@uom.lk>
2024-10-01 00:01:49 +0530

    add controller

---
+++ src/main/java/lk/ac/mrt/cse/ishara/fleet/FleetController.java
@@ -0,0 +1,13 @@
+package lk.ac.mrt.cse.ishara.fleet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FleetController {
+
+    @GetMapping("/index")
+    public String goHome() {
+        return "index";
+    }
+}
