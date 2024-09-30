d045f90cdc1b34be36f0a69906cffaa72220dd50
IsharaU <wellahewawiud.20@uom.lk>
2024-09-30 12:52:51 +0530

    add client code

--- src/main/java/lk/ac/mrt/cse/ishara/fleet/models/Client.java
+++ src/main/java/lk/ac/mrt/cse/ishara/fleet/models/Client.java
@@ -1,4 +1,42 @@
 package lk.ac.mrt.cse.ishara.fleet.models;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    private String name;
+    private String address;
+    private String city;
+    private String phone;
+    private String mobile;
+    private String website;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name="countryId", insertable=false, updatable=false)
+    private Country country;
+    private Integer countryid;
+
+    @ManyToOne
+    @JoinColumn(name="stateId", insertable=false, updatable=false)
+    private State state;
+    private Integer stateId;
+
+    private String details;
 }
+
