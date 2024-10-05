df660e5d80d2f7517eb62c3b7d4337ba8b7454a6
IsharaU <wellahewawiud.20@uom.lk>
2024-10-05 10:58:19 +0530

    create controller package

--- src/main/java/lk/ac/mrt/cse/ishara/fleet/controllers/FleetController.java
+++ src/main/java/lk/ac/mrt/cse/ishara/fleet/controllers/FleetController.java
@@ -1,4 +1,4 @@
-package lk.ac.mrt.cse.ishara.fleet;
+package lk.ac.mrt.cse.ishara.fleet.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
